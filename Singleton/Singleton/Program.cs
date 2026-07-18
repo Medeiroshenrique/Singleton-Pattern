03c2a9f33903a6d483c067b808e075a7aae6570e
Henrique  Medeiros <102472768+Medeiroshenrique@users.noreply.github.com>
2026-07-18 14:28:10 -0300

    Fix singleton instance retrieval method

--- Singleton/Singleton/Program.cs
+++ Singleton/Singleton/Program.cs
@@ -6,13 +6,13 @@ namespace Singleton
     {
         public static void Main(string[] args)
         {
-            Singleton WhithTheInstance = Singleton.GetInstance;
+            Singleton WhithTheInstance = Singleton.Instance;
             WhithTheInstance.Message("I'm with the single instance!!!");
 
-            Singleton OtherWhoGotTheInstance = Singleton.GetInstance;
+            Singleton OtherWhoGotTheInstance = Singleton.Instance;
             OtherWhoGotTheInstance.Message("Okay, I got the single instance!");
 
-            Singleton ThirdWhoGotTheInstance = Singleton.GetInstance;
+            Singleton ThirdWhoGotTheInstance = Singleton.Instance;
             ThirdWhoGotTheInstance.Message("I got it! The instance is with me! ");
         }
     }
